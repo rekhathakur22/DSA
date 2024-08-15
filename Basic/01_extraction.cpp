d435b96bd0e4bcb9fe95f119103e353969ef2799
Rekha <rrekhathakur222@gmail.com>
2024-08-15 11:35:21 +0530

    Basic started

---
+++ Basic/01_extraction.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  vector<int> extractDigit(int num)
+  {
+    int N = num;
+
+    // store extracted digits
+    vector<int> digit;
+
+    while (N > 0)
+    {
+      // extract last digit
+      int lastDigit = N % 10;
+
+      digit.push_back(lastDigit);
+
+      N = N / 10;
+    }
+    return digit;
+  }
+};
+
+int main()
+{
+  Solution sol;
+
+  int num;
+  vector<int> result;
+
+  // take input from user
+  cin >> num;
+
+  result = sol.extractDigit(num);
+
+  // print all digits present in num
+  // print the digit in reverse order
+  for (int num : result)
+  {
+    cout << num << " ";
+  }
+  cout << endl;
+  return 0;
+}
+
+/*
+time complexity : O(number of digit);
+ loop is run until all digit not extracted'
+
+ space complexity : O(number of digit);
+ extra space reqired for storing the extracted digit
+ this extra space is equal to no of digits present in the number
+ */
