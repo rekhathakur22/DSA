741afb56aa30fa38fee91d8de99c3e9d8aaed91b
Rekha <rrekhathakur222@gmail.com>
2024-08-16 22:17:57 +0530

    basic math day 2

--- BasicMath/02_countEvenlyDivideDigit.cpp
+++ BasicMath/02_countEvenlyDivideDigit.cpp
@@ -6,9 +6,13 @@ class Solution
 public:
   int evenlyDivide(int N)
   {
+
     int num = N;
     int count = 0;
 
+    // num is changed based on removel of last digit
+    // if lastdigit is not zero and it divides the given number evenly
+    // such that remainder is zero then we increase the count by one
     while (num > 0)
     {
       int lastDigit = num % 10;
@@ -26,12 +30,15 @@ public:
 int main()
 {
   Solution sol;
+  //  t is the number of test cases
   int t;
+  // take the input for test case
   cin >> t;
 
   while (t--)
   {
     int N;
+    // take the input
     cin >> N;
     cout << sol.evenlyDivide(N) << endl;
   }
